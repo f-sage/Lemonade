65eb841475cda326acfd4940e1325cc6b768dcf4
f-sage <junia.en@gmail.com>
2025-11-18 17:28:44 +0200

    Create local database when user opens the app

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,39 +5,56 @@ import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { SQLiteDatabase, SQLiteProvider } from "expo-sqlite";
+
+const DB_NAME = "lemonade.db"
+
+const createDbIfNeeded = async (db: SQLiteDatabase) => {
+  console.log("Creating database");
+  try {
+    // Create a table
+    const response = await db.execAsync(
+      "CREATE TABLE IF NOT EXISTS textentries (id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT)"
+    );
+    console.log("Database created", response);
+  } catch (error) {
+    console.error("Error creating database:", error);
+  }
+};
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+    <SQLiteProvider databaseName={DB_NAME} onInit={createDbIfNeeded}>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+          headerShown: false,
+          tabBarButton: HapticTab,
+        }}>
         <Tabs.Screen
-        name="add-text-entry"
-        options={{
-          title: 'Add entry',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-      
-    </Tabs>
+          name="index"
+          options={{
+            title: 'Home',
+            tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="explore"
+          options={{
+            title: 'Explore',
+            tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          }}
+        />
+          <Tabs.Screen
+          name="add-text-entry"
+          options={{
+            title: 'Add entry',
+            tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          }}
+        />
+      </Tabs>
+    </SQLiteProvider>
   );
 }
