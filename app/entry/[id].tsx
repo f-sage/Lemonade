0bdcaf6431b71190e768bf5a3cb433f8cc0e7abd
f-sage <junia.en@gmail.com>
2025-11-20 16:32:40 +0200

    Adjust styles for View Entry

--- app/entry/[id].tsx
+++ app/entry/[id].tsx
@@ -1,9 +1,11 @@
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
-import TextEntry, { emptyTextEntry } from '@/models/TextEntry';
+import { TextEntry, emptyTextEntry } from '@/models/TextEntry';
 import { useLocalSearchParams } from 'expo-router';
 import { useSQLiteContext } from 'expo-sqlite';
 import { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
+
 
 export const ViewEntryScreen = () => {
   const { id } = useLocalSearchParams<{ id: string }>();
@@ -26,13 +28,35 @@ export const ViewEntryScreen = () => {
       getFullEntry();
     },[id])
 
+    const datetime = new Date(entry.datetime).toLocaleString();
+
   return (
-    <ThemedView>
-      <ThemedText selectable={true}>
-        {entry.text}
+    <ThemedView style={styles.wrapper}>
+      <ThemedText style={styles.date}>{datetime}</ThemedText>
+      <ThemedText 
+        selectable={true}
+        style={styles.text}
+      >
+      {entry.text}
       </ThemedText>
     </ThemedView>
   )
 };
+
+const styles = StyleSheet.create({
+  date: {
+   fontStyle:'italic'
+  },
+   wrapper: {
+    flex: 1,
+    paddingVertical:4,
+    paddingHorizontal:16,
+    overflow: 'hidden',
+  },
+  text:{
+    marginTop: 8,
+    textAlign: "justify", //as of 2025-11-20, this does not work with selectable but hoping for the best!! 
+  }
+});
 
 export default ViewEntryScreen;
