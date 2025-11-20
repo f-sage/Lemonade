e28767cc94b6f77a2976c406bc0563ee748e404f
f-sage <junia.en@gmail.com>
2025-11-20 15:05:35 +0200

    Add a new page for viewing a text entry

---
+++ app/entry/[id].tsx
@@ -0,0 +1,17 @@
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+import { useLocalSearchParams } from 'expo-router';
+
+export const ViewEntryScreen = () => {
+  const { id } = useLocalSearchParams<{ id: string }>();
+
+  return (
+    <ThemedView>
+      <ThemedText>
+        entry id: {id}
+      </ThemedText>
+    </ThemedView>
+  )
+};
+
+export default ViewEntryScreen;
