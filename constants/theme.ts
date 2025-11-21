25d3d825b78c2b046adb36e3accdeee982743db8
f-sage <junia.en@gmail.com>
2025-11-21 12:40:55 +0200

    Use list item background based on the theme color

--- constants/theme.ts
+++ constants/theme.ts
@@ -16,6 +16,7 @@ export const Colors = {
     icon: '#687076',
     tabIconDefault: '#687076',
     tabIconSelected: tintColorLight,
+    transparentBackground:'rgba(10, 126, 164,0.3)'
   },
   dark: {
     text: '#ECEDEE',
@@ -24,6 +25,7 @@ export const Colors = {
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',
     tabIconSelected: tintColorDark,
+    transparentBackground:'rgba(255,255,255,0.3)'
   },
 };
 
