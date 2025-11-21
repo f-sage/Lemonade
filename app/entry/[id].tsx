6c13f7d86806540d34a31942eaa307c1af8fc62a
f-sage <junia.en@gmail.com>
2025-11-21 11:43:24 +0200

    Fix react-hooks/exhaustive-deps

--- app/entry/[id].tsx
+++ app/entry/[id].tsx
@@ -12,21 +12,24 @@ export const ViewEntryScreen = () => {
   const database = useSQLiteContext();
   const [entry, setEntry] = useState<TextEntry>(emptyTextEntry);
 
-   const getFullEntry = async ()=>{
-      try {
-        const response = await database.getFirstAsync<TextEntry>(
-          `SELECT text, datetime FROM textentries WHERE id = ? `, [id]
-        );
-        console.log("Entry retrieved successfully:", response);
-        setEntry(response ?? emptyTextEntry)
-      } catch (error) {
-        console.error("Error retrieving entry from the DB:", error);
-      }
-    }
 
-    useEffect(()=>{
+    useEffect(()=>{ 
+    const getFullEntry = async ()=>{
+        try {
+          const response = await database.getFirstAsync<TextEntry>(
+            `SELECT text, datetime FROM textentries WHERE id = ? `, [id]
+          );
+          console.log("Entry retrieved successfully:", response);
+          setEntry(response ?? emptyTextEntry)
+        } catch (error) {
+          console.error("Error retrieving entry from the DB:", error);
+        }
+      }
+
       getFullEntry();
-    },[id])
+    },
+    [id, database]
+  )
 
     const datetime = new Date(entry.datetime).toLocaleString();
 
