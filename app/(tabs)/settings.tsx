0fc01b1b29af35479277ba88b5362929305e747d
f-sage <junia.en@gmail.com>
2025-11-22 21:30:59 +0200

    Style buttons for language options to be able to see the press

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,10 +1,11 @@
+import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
 import { LANGUAGE_KEY } from "@/i18n";
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import React from 'react';
 import { useTranslation } from "react-i18next";
-import { Button, StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 const SettingsScreen = () => {
@@ -20,8 +21,25 @@ const SettingsScreen = () => {
         <SafeAreaView style={styles.paddedWrapper}>
           <ThemedView style={styles.paddedWrapper}>
             <Text>{t('settings.language')}</Text> 
-            <Button title="English" onPress={() => changeLanguageSetting('en-US')} />
-            <Button title="Ukrainian" onPress={() => changeLanguageSetting('uk-UA')} /> 
+
+            <TouchableOpacity 
+              onPress={() => changeLanguageSetting('en-US')}
+              style={styles.button}
+            >
+              <ThemedText style={styles.buttonText}>
+                English
+              </ThemedText> 
+            </TouchableOpacity>
+            
+            <TouchableOpacity
+              onPress={() => changeLanguageSetting('uk-UA')} 
+              style={styles.button}
+            >
+              <ThemedText style={styles.buttonText}>
+                Ukrainian
+              </ThemedText>
+            </TouchableOpacity>
+            
           </ThemedView>
         </SafeAreaView>
       </SafeAreaProvider>
@@ -33,6 +51,17 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingVertical:16,
     paddingHorizontal:16,
+  },
+  button:{
+    backgroundColor:'#bb32ed',
+    padding:8,
+    margin:2,
+    alignItems: 'center',
+  },
+  buttonText: {
+    textTransform: 'uppercase',
+    color:'white',
+    fontWeight:'bold'
   }
 });
 
