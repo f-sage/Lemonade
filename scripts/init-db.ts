96f71b6542cd9753189d103decb1fd67d020846c
f-sage <junia.en@gmail.com>
2025-12-02 14:46:58 +0200

    Create tables for surveys and interviews

--- scripts/init-db.ts
+++ scripts/init-db.ts
@@ -3,13 +3,60 @@ import { SQLiteDatabase } from "expo-sqlite";
 export const DB_NAME = "lemonade.db"
 
 export const createDbIfNeeded = async (db: SQLiteDatabase) => {
-  console.log("Creating database");
+  console.log("Creating database...");
   try {
-    // Create a table
-    const response = await db.execAsync(
+    // text entries
+    await db.execAsync(
       "CREATE TABLE IF NOT EXISTS textentries (id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT, createdAt TEXT)"
     );
-    console.log("Database created", response);
+
+    // survey 
+    await db.execAsync(
+      "CREATE TABLE IF NOT EXISTS surveys (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, createdAt TEXT)"
+    );
+
+    // survey revision (immutable)
+    // a new survey revision is created if the user wishes to update a survey
+    await db.execAsync(
+      "CREATE TABLE IF NOT EXISTS survey_revisions "
+      + "(id INTEGER PRIMARY KEY AUTOINCREMENT, createdAt TEXT, surveyId NUMBER, "
+      + "FOREIGN KEY(surveyId) REFERENCES surveys(id))"
+    );
+
+    // questions belong to survey revisions
+    // answerType = 'option' | 'text'
+    await db.execAsync(
+      "CREATE TABLE IF NOT EXISTS survey_questions "
+      + "(id INTEGER PRIMARY KEY AUTOINCREMENT, surveyId INTEGER, createdAt TEXT, "
+      + "text TEXT, answerType TEXT, "
+      + "FOREIGN KEY(surveyRevisionId) REFERENCES survey_revisions(id))"
+    );
+
+    // answer options for questions that have answerType = "option"
+    await db.execAsync(
+      "CREATE TABLE IF NOT EXISTS answer_options "
+      + "(id INTEGER PRIMARY KEY AUTOINCREMENT, surveyQuestionId INTEGER, createdAt TEXT, "
+      + "text TEXT,"
+      + "FOREIGN KEY(surveyQuestionId) REFERENCES survey_questions(id))"
+    );
+
+    // a collection of user's answers to questions in one survey
+     await db.execAsync(
+      "CREATE TABLE IF NOT EXISTS interviews "
+      + "(id INTEGER PRIMARY KEY AUTOINCREMENT, surveyQuestionId INTEGER, createdAt TEXT)"
+    )
+    
+    // user's answer to one question of a survey
+    // belongs to a question and to an interview
+    await db.execAsync(
+      "CREATE TABLE IF NOT EXISTS answers "
+      + "(id INTEGER PRIMARY KEY AUTOINCREMENT, surveyQuestionId INTEGER, createdAt TEXT, "
+      + "text TEXT, interviewId NUMBER, "
+      + "FOREIGN KEY(surveyQuestionId) REFERENCES survey_questions(id), "
+      + "FOREIGN KEY(interviewId) REFERENCES interviews(id))"
+    );
+
+    console.log("Database created");
   } catch (error) {
     console.error("Error creating database:", error);
   }
